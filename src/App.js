5cd4d032de72ca8960800eb348ea26654aeededa
deeparka <arkadeep180@gmail.com>
2022-12-05 03:38:30 +0530

    redux with react

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment, logCheck } from "./actions";
+
 function App() {
+  const counter = useSelector(state => state.counter)
+
+  const isLogged = useSelector(state => state.isLogged)
+
+  const dispatch = useDispatch()
+
   return (
     <div className="App">
-      Hello There
+      <button onClick={() => dispatch(increment(2))}>+</button>
+      <h1>Counter: {counter}</h1>
+        <button onClick={() => dispatch(decrement())}>-</button>
+
+      {isLogged ? <h5>"Logged In"</h5> : <h5>"Not Logged In"</h5>}
+      <button onClick={() => dispatch(logCheck())}>Click Me!</button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,46 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { createStore } from 'redux';
+import allReducers from './reducers'
+import { Provider } from 'react-redux';
 
-// STORE -> GLOBALIZED STATE
-
-// ACTION INCREMENT
-const increment = () => {
-  return {
-    type: 'INCREMENT'
-  }
-}
-
-// ACTION DECREMENT
-const decrement = () => {
-  return {
-    type: 'DECREMENT'
-  }
-}
-
-// REDUCER
-const counter = (state = 0, action) => {
-  switch(action.type) {
-    case 'INCREMENT':
-      return state + 1
-    case 'DECREMENT':
-      return state - 1
-  }
-}
-
-let store = createStore(counter)
-
-store.subscribe(() => console.log(store.getState()))
-
-// DISPATCH
-store.dispatch(increment())
-store.dispatch(increment())
-store.dispatch(decrement())
+const store = createStore(allReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
